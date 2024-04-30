0fd8a6f4163712df2e5e99735c0423751dce46c8
XrayChiken <hannibal1209@aol.com>
2024-04-30 11:11:23 -0400

    Added SFX, Music, Events and Soundbanks into WWise

--- Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,14 +13,37 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_BROKENREACTORSFX = 2901348110U;
-        static const AkUniqueID PLAY_REACTORSFX = 623613221U;
+        static const AkUniqueID OUT_OF_OXYGEN_AMBIENT = 1891082781U;
+        static const AkUniqueID OUT_OF_OXYGEN_WOOSH = 2546328017U;
+        static const AkUniqueID PLAY_AMBIENT_BROKEN_REACTOR = 1398985945U;
+        static const AkUniqueID PLAY_AMBIENT_REACTOR = 1555987701U;
+        static const AkUniqueID PLAY_HAND_CONTACT_01 = 2382057926U;
+        static const AkUniqueID PLAY_HAND_CONTACT_02 = 2382057925U;
+        static const AkUniqueID PLAY_HAND_CONTACT_03 = 2382057924U;
+        static const AkUniqueID PLAY_HAND_CONTACT_04 = 2382057923U;
+        static const AkUniqueID PLAY_HAND_CONTACT_05 = 2382057922U;
+        static const AkUniqueID PLAY_HAND_CONTACT_06 = 2382057921U;
+        static const AkUniqueID PLAY_HAND_CONTACT_07 = 2382057920U;
+        static const AkUniqueID PLAY_HAND_CONTACT_08 = 2382057935U;
+        static const AkUniqueID PLAY_INSIDE_BGM = 1587063413U;
+        static const AkUniqueID PLAY_JETPACK_HISS = 4046988224U;
+        static const AkUniqueID PLAY_LOW_OXYGEN_WARNING = 4213574854U;
+        static const AkUniqueID PLAY_MENU_BGM = 1876933864U;
+        static const AkUniqueID PLAY_METAL_SLAP_01 = 136522678U;
+        static const AkUniqueID PLAY_METAL_SLAP_02 = 136522677U;
+        static const AkUniqueID PLAY_OUTSIDE_BGM = 2993185784U;
+        static const AkUniqueID PLAY_OXYGEN_FILLED = 4024519181U;
+        static const AkUniqueID PLAY_REACTOR_FIXED_INDICATOR = 1098601461U;
+        static const AkUniqueID PLAY_UI_BUTTON_CLICK = 1661558166U;
+        static const AkUniqueID PLAY_UI_BUTTON_HOVER_OFF = 3400169570U;
+        static const AkUniqueID PLAY_UI_BUTTON_HOVER_ON = 4085758564U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID REACTOR = 2204642885U;
+        static const AkUniqueID MUSIC_SOUNDBANK = 3589812408U;
+        static const AkUniqueID SFX_SOUNDBANK = 2641024368U;
     } // namespace BANKS
 
     namespace BUSSES
