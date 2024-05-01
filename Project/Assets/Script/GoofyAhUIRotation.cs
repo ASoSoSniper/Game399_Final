6563e262b9bd3c45a2b54c45e47f4605eb50ff33
JustAGuyNamedCy <cypabeye@gmail.com>
2024-05-01 18:18:46 -0400

    Get Rotated LMAO

---
+++ Project/Assets/Script/GoofyAhUIRotation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoofyAhUIRotation : MonoBehaviour
+{
+    public Canvas UIToRotate;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
